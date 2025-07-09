fbd6269294173204becaa9a23b77dc8898b34fcf
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-07-09 16:03:55 +0600

    previous commit message erroneous, 11.11 and 11.12 should be 11.10 and 11.11 respectively. 11.12 test push

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  // if (true) throw('error...  ')
+  res.send('ok')
+})
