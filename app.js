fe1cefe8ab0d4bb1c4d5b183e712ba8720c805cb
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-07-09 12:38:00 +0600

    excercise 11.6 some or all fixes applied according to eslint

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable
-console.log(process.env);
-const PORT = process.env.PORT || 8080;
+// eslint-disable-next-line
+console.log(process.env)
+const PORT = process.env.PORT || 8080
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  // eslint-disable-next-line
+  console.log(`server started on port ${PORT}`)
+})
