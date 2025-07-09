08752ff949a0a21ddadda15791ac26894f227ebb
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-07-09 13:44:17 +0600

    eslint expected LF errors fixed for pokedex.cy.js and .eslintrc.js and eslint errors fixed for cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      // eslint-disable-next-line
+      console.log(on, config)
       // implement node event listeners here
     },
   },
-});
+})
